bec36378f5d461aadf5ee79f0f547691c51bf033
AwfulWaffleMining <awfulwafflemining@gmail.com>
2026-05-23 04:53:06 -0500

    fix: startos/interfaces.ts

--- startos/interfaces.ts
+++ startos/interfaces.ts
@@ -1,19 +1,17 @@
 import { sdk } from './sdk'
 
 export const setInterfaces = sdk.setupInterfaces(async ({ effects }) => {
-  const host = sdk.MultiHost.of(effects, 'main')
-
-  const stratumOrigin = await host.bindPort(4444, {
+  const stratumMulti = sdk.MultiHost.of(effects, 'stratum')
+  const stratumOrigin = await stratumMulti.bindPort(4444, {
     protocol: null,
     addSsl: null,
     preferredExternalPort: 4444,
     secure: { ssl: false },
   })
-
   const stratum = sdk.createInterface(effects, {
     name: 'Stratum Mining',
     id: 'stratum',
-    description: 'Point your SHA-256 miners here',
+    description: 'Point your SHA-256 miners here. Use worker name as your BCH address.',
     type: 'api',
     masked: false,
     schemeOverride: null,
@@ -21,7 +19,6 @@ export const setInterfaces = sdk.setupInterfaces(async ({ effects }) => {
     path: '',
     query: {},
   })
-
   const stratumReceipt = await stratumOrigin.export([stratum])
 
   return [stratumReceipt]
