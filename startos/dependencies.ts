581362f3cb375aa0dde74ca73c4193b4ee04232b
AwfulWaffleMining <awfulwafflemining@gmail.com>
2026-05-23 04:53:07 -0500

    fix: startos/dependencies.ts

--- startos/dependencies.ts
+++ startos/dependencies.ts
@@ -1,11 +1,9 @@
 import { sdk } from './sdk'
 
-export const setDependencies = sdk.setupDependencies(async ({ effects }) => {
-  return {
-    'bitcoin-cash': {
-      kind: 'running',
-      versionRange: '>=29.0.1:0',
-      healthChecks: ['bitcoind'],
-    },
-  }
-})
+export const setDependencies = sdk.setupDependencies(async ({ effects }) => ({
+  'bitcoin-cash': {
+    kind: 'running' as const,
+    versionRange: '>=29.0.1:0',
+    healthChecks: [],
+  },
+}))
