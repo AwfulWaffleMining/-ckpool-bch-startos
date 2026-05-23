9f18e566562fd1d6b13cc0e94163d16ef6cdca8c
AwfulWaffleMining <awfulwafflemining@gmail.com>
2026-05-23 04:53:05 -0500

    fix: startos/main.ts

--- startos/main.ts
+++ startos/main.ts
@@ -1,12 +1,7 @@
 import { sdk } from './sdk'
-import { setInterfaces } from './interfaces'
-import { setDependencies } from './dependencies'
 
 export const main = sdk.setupMain(async ({ effects }) => {
   console.info('Starting CKPool BCH...')
-
-  await setInterfaces(effects)
-  await setDependencies(effects)
 
   return sdk.Daemons.of(effects).addDaemon('ckpool', {
     subcontainer: await sdk.SubContainer.of(
@@ -24,12 +19,11 @@ export const main = sdk.setupMain(async ({ effects }) => {
       command: ['/usr/local/bin/docker_entrypoint.sh'],
     },
     ready: {
-      display: 'CKPool BCH Stratum',
-      gracePeriod: 30_000,
+      display: 'Stratum Server',
       fn: () =>
         sdk.healthCheck.checkPortListening(effects, 4444, {
           successMessage: 'Stratum server is ready on port 4444',
-          errorMessage: 'Stratum port 4444 is not yet open',
+          errorMessage: 'Stratum port 4444 not yet open',
         }),
     },
     requires: [],
